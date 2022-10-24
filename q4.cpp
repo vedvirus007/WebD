1058533628a5ef02e99b47ba0b563d8cd8167ad9
zeeshangengar1 <57321494+zeeshangengar1@users.noreply.github.com>
2022-10-24 13:14:55 +0530

    Upload !

---
+++ q4.cpp
@@ -0,0 +1,50 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define yes cout<<"YES\n";
+#define no cout<<"NO\n";
+#define fl(n) for(int i=0;i<n;i++)
+#define flj(n) for(int j=0;j<n;j++)
+#define flo(n) for(int i=1;i<=n;i++)
+#define mii map<int,int>
+#define mci map<char,int>
+#define V vector<int>
+#define vp vector<pair<int,int>>
+#define pb push_back
+#define pp pair<int,int>
+#define ff first
+#define ss second
+#define S set<int>
+#define all(v) v.begin(),v.end()
+#define gr greater<int>()
+#define show2(a, b) cout<<a<<' '<<b<<endl;
+#define show3(a, b, c) cout<<a<<' '<<b<<' '<<c<<endl;
+const long long mod=1000000007;
+const int N=1e5+5;
+const long double PI=3.14159265358;
+vector<vector<int>> adj;
+vector<bool> vis;
+vector<int> val;
+vector<int> gld;
+
+int32_t main(){
+int t;
+cin>>t;
+while(t--)
+{
+    int n,k;
+    cin>>n>>k;
+    int a[n];
+    int mn=INT_MIN,mni=-1;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+        if(mn>)
+    }
+    int j,k;
+    j=
+}
+
+
+}
